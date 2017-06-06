e4616ba0c8acc5aa1b4dc21c4489b7dab31bf58c
WongKaiSung <kaisung_26@hotmail.com>
2017-06-06 10:17:41 +0800

    Lab2

--- app/src/main/java/my/edu/tarc/lab2_1/MainActivity.java
+++ app/src/main/java/my/edu/tarc/lab2_1/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, SecondActivity.class);
         intent.putExtra("MSG", msg);
         startActivity(intent);
+        //test
 
     }
 }
